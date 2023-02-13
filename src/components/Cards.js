20b8fc6ad6c9454cb986acd536680019e82c0af8
Sho Neriki <ikirenohs@gmail.com>
2023-02-13 21:25:53 +0900

    fixed bug of not being able to click on a card when it overlaps with .scoreDivLayout

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -62,29 +62,38 @@ const Cards = () => {
 
 
   const handleClick = (e, card) => {
+    // 1.) set cardName to the name in the card object from cardsInfo
     const cardName = card.name
     if (cardNames.includes(cardName)) {
+      // 7.) set duplicate to true, as there is a duplication in the array of cardNames
       setDuplicate(true)
+      // 8.) set/reset the score to 0
       setScore(0)
+      // 9.) set/reset the cardNames array to an empty array
       setCardNames([])
     } else {
+      // 2.) set duplicate to false, as there are no duplications yet in the array cardNames (which is set to an empty array above
       setDuplicate(false)
+      //3.) set cardNames array to a new array that interpolates the existing cardNames array with the cardName in 1 pushed into it
       setCardNames([...cardNames, cardName])
+      // 4.) set score to the current score + 1
       setScore(score + 1)
+      // 5.) if the score is greater than the best score, then set the best score to the score
       if (score + 1 > bestScore) {
         setBestScore(score + 1);
       }
     }
+    // 6.) shuffle the cards
     shuffleCards(cardsInfo)
   }
 
   const shuffleCards = (cards) => {
-  for (let i = cards.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [cards[i], cards[j]] = [cards[j], cards[i]];
+    for (let i = cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [cards[i], cards[j]] = [cards[j], cards[i]];
+    }
+    setCardsInfo(cards)
   }
-  setCardsInfo(cards)
-}
 
 
   return (
