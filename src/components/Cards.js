b12a06d0a99bcd771f26f01f9a2ee356479c92c4
Sho Neriki <ikirenohs@gmail.com>
2023-02-13 22:00:06 +0900

    deleted unnecessary comments and spaces

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,45 +4,6 @@ import {cardsInfo} from "./CardsInfo"
 import "../App.css"
 
 const Cards = () => {
-  /*
-    baby-luigi,
-    baby-mario,
-    baby-yoshi,
-    bowser,
-    bowser-jr,
-    daisy,
-
-    kamek,
-    larry-koopa,
-    ludwig-von-koopa,
-    luigi,
-    mario,
-    peach,
-
-    professor-egad,
-    toad,
-    toadsworth,
-    waluigi,
-    wario,
-    yoshi,
-  */
-
-  // in terms of layout:
-  // 1. create a grid of cards of 2x9, (3x6, 4x4), for mobile, (tablet and desktop)
-  // 2. each card will have
-      //an image,
-      // a name and
-      // a click event listener
-
-  // 3. when a card is clicked,
-      // the id of the card should be stored in an array, so push the name into the array
-      // cards should be shuffled on screen
-  // 4. there should be a state for adding score for each card in the array
-    // (adding 1 to the score for each click)
-  // 5. if card is clicked with the same id as the id in the card id array,
-      //then the score should be reset to 0
-  // 6. if the score is greater than the best score,
-      //then the best score should be updated to the score
 
   const [cardNames, setCardNames] = useState([])
 
@@ -59,8 +20,6 @@ const Cards = () => {
     setShowExplanation(!showExplanation)
   }
 
-
-
   const handleClick = (e, card) => {
     // 1.) set cardName to the name in the card object from cardsInfo
     const cardName = card.name
@@ -74,7 +33,7 @@ const Cards = () => {
     } else {
       // 2.) set duplicate to false, as there are no duplications yet in the array cardNames (which is set to an empty array above
       setDuplicate(false)
-      //3.) set cardNames array to a new array that interpolates the existing cardNames array with the cardName in 1 pushed into it
+      // 3.) set cardNames array to a new array that interpolates the existing cardNames array with the cardName in 1 pushed into it
       setCardNames([...cardNames, cardName])
       // 4.) set score to the current score + 1
       setScore(score + 1)
